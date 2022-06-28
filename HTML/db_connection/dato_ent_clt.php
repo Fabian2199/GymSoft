a48b3fd35d9aaf05e2b41360d2b91ec23ed36c09
Fabian2199 <neyder.rodriguez@uptc.edu.co>
2022-06-28 09:43:47 -0500

    commit

--- HTML/db_connection/dato_ent_clt.php
+++ HTML/db_connection/dato_ent_clt.php
@@ -8,7 +8,7 @@
   function get_datos($id_persona){
     include ('connection.php');
     $slq_datos = "SELECT * FROM personas p JOIN usuarios u ON p.id_persona = u.id_persona JOIN facturas f ON f.id_cliente = u.id_user JOIN detalles_fac d ON d.id_factura = f.id_factura 
-    WHERE f.fecha_nac = (SELECT MAX(f.fecha_nac) FROM facturas f JOIN usuarios u ON f.id_cliente = u.id_user JOIN personas p ON u.id_persona = p.id_persona WHERE p.id_persona =".$id_persona.")";
+    WHERE f.fecha_fac = (SELECT MAX(f.fecha_fac) FROM facturas f JOIN usuarios u ON f.id_cliente = u.id_user JOIN personas p ON u.id_persona = p.id_persona WHERE p.id_persona =".$id_persona.")";
     $consulta_datos = $conexion->query($slq_datos);
     return $consulta_datos;
   };
