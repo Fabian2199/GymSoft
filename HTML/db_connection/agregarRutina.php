65edcdcd086984feab5ec47bddd775ba60db4da4
CamilSan <2241jose@gmail.com>
2022-07-20 09:32:51 -0500

    Modificación de logica para agregar una rutina

--- HTML/db_connection/agregarRutina.php
+++ HTML/db_connection/agregarRutina.php
@@ -1,18 +1,23 @@
 <?php
     include 'connection.php';
-    $entrenador = 'ent164897526'; //<----------- Dónde se almacena el user de la sesión
-    $cliente = $_POST['clientes'];
+    //include 'login.php';
+    //$entrenador = $usuario;
+    $entrenador = 'ent1265986326'; //<----------- Dónde se almacena el user de la sesión
+    $cliente = 'clt'.$_POST['clientes'];
     $ejercicio = $_POST['ejercicios'];
     $dia = $_POST['dias'];
     $series = $_POST['num_series'];
     $repeticiones = $_POST['num_repeticiones'];
 
     //echo "cli={$cliente} ejer={$ejercicio} dia={$dia} series={$series} rep={$repeticiones} ";
+    //echo "Entrenador: ".$entrenador;
 
+    ///*
     $query = "INSERT INTO rutinas VALUES('$entrenador', '$cliente', '$ejercicio', '$dia', '$series', '$repeticiones');";
     $consulta = mysqli_query($conexion, $query);
     
     if($consulta){
-        Header("Location: ../agregar_rutina.php");
+        Header("Location: ../ent_index.php");
     }
+    //*/
 ?>
