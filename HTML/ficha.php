06d7ead402d2c22af9cf2b094d5713427752ba99
Tian1705 <sebastiandro99@gmail.com>
2022-07-23 18:05:40 -0500

    Update ficha.php

--- HTML/ficha.php
+++ HTML/ficha.php
@@ -47,11 +47,10 @@
       <img alt="" class="u-expanded-height u-image u-image-default u-image-1" data-image-width="1000" data-image-height="1502" src="img/lateral.jpg">
       <div class="u-align-left u-container-style u-group u-group-1">
         <div class="u-container-layout u-valign-top u-container-layout-1">
-          <a href="ent_index.php" class="boton">Regresar</a>
+            <a href="ent_index.php" class="boton">Regresar</a>
           <h1 class="u-align-center u-custom-font u-font-playfair-display u-text u-text-1">Ficha Antropometrica</h1>
           <h5 class="u-align-center u-text u-text-2">Rellene todos los campos</h5>
           <div class="u-expanded-width u-form u-form-1">
-            /*Formulario con los campos correspondientes a la ficha_antropometrica*/
             <form action="#" method="POST" class="u-clearfix u-form-spacing-30 u-form-vertical u-inner-form" style="padding: 0;" name="form">
               <div class="u-form-group u-form-select u-form-group-1">
                 <label for="select-0c1a" class="u-label">Cliente</label>
@@ -299,7 +298,7 @@
               </div>
               <div class="u-align-left u-form-group  u-form-group-42">
                 <a href="ent_index.php" class="boton">Regresar</a>
-                <input type="submit" name="register" class="registrar u-btn u-btn-submit u-button-style u-hover-grey-25 u-white u-btn-1" id="registrar" onclick="location.href='ent_index.php';"/>
+                <input type="submit" onclick="pregunta()" name="register" class="registrar u-btn u-btn-submit u-button-style u-hover-grey-25 u-white u-btn-1" id="registrar"/>
               </div>
             </form>
 
@@ -317,10 +316,10 @@
 
     <footer class="u-clearfix u-footer u-grey-80" id="sec-d26a"><div class="u-clearfix u-sheet u-sheet-1">
         <div class="u-align-left u-social-icons u-spacing-10 u-social-icons-1">
-          <a class="u-social-url" title="facebook" target="_blank" href="https://www.facebook.com/BfreeGym"><span class="u-icon u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-f24d"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-f24d"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
+          <a class="u-social-url" title="facebook" target="_blank" href=""><span class="u-icon u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-f24d"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-f24d"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
             c0-6.7,3.1-17,17-17h12.5v13.9H73.5z"></path></svg></span>
           </a>
-          <a class="u-social-url" title="instagram" target="_blank" href="https://www.instagram.com/bfreegym/"><span class="u-icon u-social-icon u-social-instagram u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-2d0e"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-2d0e"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
+          <a class="u-social-url" title="instagram" target="_blank" href=""><span class="u-icon u-social-icon u-social-instagram u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-2d0e"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-2d0e"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
             z M55.9,66.4c-5.7,0-10.3-4.6-10.3-10.3c-0.1-5.7,4.6-10.3,10.3-10.3c5.7,0,10.3,4.6,10.3,10.3C66.2,61.8,61.6,66.4,55.9,66.4z"></path><path fill="#FFFFFF" d="M74.3,33.5c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2S76.6,33.5,74.3,33.5z"></path><path fill="#FFFFFF" d="M73.1,21.3H38.6c-9.7,0-17.5,7.9-17.5,17.5v34.5c0,9.7,7.9,17.6,17.5,17.6h34.5c9.7,0,17.5-7.9,17.5-17.5V38.8
             C90.6,29.1,82.7,21.3,73.1,21.3z M83,73.3c0,5.5-4.5,9.9-9.9,9.9H38.6c-5.5,0-9.9-4.5-9.9-9.9V38.8c0-5.5,4.5-9.9,9.9-9.9h34.5
             c5.5,0,9.9,4.5,9.9,9.9V73.3z"></path></svg></span>
@@ -329,3 +328,11 @@
       </div></footer>
 
 </body></html>
+
+<script language="JavaScript">
+function pregunta(){
+    if (alert('¡Formulario registrado con exito!')){
+       document.tuformulario.submit()
+    }
+}
+</script>
